1d016460e5146bc54b73712b4ba4375c2e336b4a
Moreen Nampeera <nampeeramoreenp@gmail.com>
2023-11-29 18:37:13 +0400

    added stop watch

--- src/components/stopwatch.jsx
+++ src/components/stopwatch.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
 
 export default function StopWatch(){
-    const [start, setStart] = useState(0);
+    const [count, setCount] = useState(0);
     return(
         <div>
           <h1>Stopwatch</h1>  
+          <div>
+            <p id='count'>{count} Seconds</p>
+            <button onClick={() => {
+            setCount(count +1);
+          }}>start</button>
+            <button>stop</button>
+            <button  onClick={() => {
+            setCount(0);
+          }}>clear</button>
+          </div>
         </div>
     );
 }
